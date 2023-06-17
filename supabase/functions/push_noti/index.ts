1fdcdf5622851dc8a4dc70d2fc7285a0da943237
Pasinun.w <pasinun.witt@kmutt.ac.th>
2023-06-18 04:01:45 +0700

    [FIX] user_journey content msg

--- supabase/functions/push_noti/index.ts
+++ supabase/functions/push_noti/index.ts
@@ -2,9 +2,7 @@ import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 import * as OneSignal from "https://esm.sh/@onesignal/node-onesignal@1.0.0-beta7";
 import * as postgres from "https://deno.land/x/postgres@v0.14.2/mod.ts";
 
-//const databaseUrl = Deno.env.get('postgresql://postgres:Paigun30062023@db.rnkpkbfkrscmxfhjxlcs.supabase.co:5432/postgres')!
-const databaseUrl =
-  "postgresql://postgres:Paigun30062023@db.rnkpkbfkrscmxfhjxlcs.supabase.co:5432/postgres";
+const databaseUrl = Deno.env.get("SUPABASE_DB_URL")!;
 const pool = new postgres.Pool(databaseUrl, 3, true);
 
 const _OnesignalAppId_ = Deno.env.get("ONESIGNAL_APP_ID")!;
@@ -19,7 +17,7 @@ const onesignal = new OneSignal.DefaultApi(configuration);
 //console.log("HEELOO TESTTEST");
 
 serve(async (req) => {
-  const { record, table } = await req.json();
+  const { record, table, type } = await req.json();
   const connection = await pool.connect();
   try {
     if (table == "board") {
@@ -63,10 +61,10 @@ serve(async (req) => {
           },
         }
       );
-    } else if (table == "user_journey") {
+    } else if (table == "user_journey" && type == "INSERT") {
       const result =
         await connection.queryObject`SELECT full_name, journey.owner FROM user_journey, journey, profile WHERE journey.journey_id = ${record.journey_id} and profile.id = ${record.user_id}`;
-      const selected = result.rows[0]; // [{ id: 1, name: "Lion" }, ...]
+      const selected = result.rows[0];
       //console.log(result);
       //console.log(selected);
       const notification = new OneSignal.Notification();
@@ -78,6 +76,49 @@ serve(async (req) => {
       notification.headings = {
         en: `New join request!`,
       };
+      const onesignalApiRes = await onesignal.createNotification(notification);
+
+      return new Response(
+        JSON.stringify({ onesignalResponse: onesignalApiRes }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization:
+              "Basic N2VkMmM1NjUtZTkyMy00ZTVkLTlhZTMtZDY2YTkzODI0YmMw",
+          },
+        }
+      );
+    } else if (table == "user_journey" && type == "UPDATE") {
+      const result =
+        await connection.queryObject`SELECT full_name FROM user_journey, journey, profile WHERE user_journey.journey_id = ${record.journey_id} and journey.journey_id = ${record.journey_id} and profile.id = journey.owner`;
+      const selected = result.rows[0];
+      console.log(result);
+      console.log(selected);
+      const notification = new OneSignal.Notification();
+      notification.app_id = _OnesignalAppId_;
+      notification.include_external_user_ids = [record.user_id];
+      if (record.status == "accepted") {
+        notification.contents = {
+          en: `The journey from ${selected.full_name} is accepted, please paid to be confirmed.`,
+        };
+        notification.headings = {
+          en: `Request accepted!`,
+        };
+      } else if (record.status == "paid") {
+        notification.contents = {
+          en: `You have paid for the journey from ${selected.full_name}, your journey request is confirmed`,
+        };
+        notification.headings = {
+          en: `Payment succeed!`,
+        };
+      }else if (record.status == "finished") {
+        notification.contents = {
+          en: `The journey from ${selected.full_name} is arrived, please complete the route.`,
+        };
+        notification.headings = {
+          en: `Journey arrived!`,
+        };
+      }
       const onesignalApiRes = await onesignal.createNotification(notification);
 
       return new Response(
@@ -161,36 +202,3 @@ serve(async (req) => {
     connection.release();
   }
 });
-
-// // Follow this setup guide to integrate the Deno language server with your editor:
-// // https://deno.land/manual/getting_started/setup_your_environment
-// // This enables autocomplete, go to definition, etc.
-// import { createClient } from '@supabase/supabase-js'
-// import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
-// console.log("Hello from Functions!")
-// const supabase = createClient('https://rnkpkbfkrscmxfhjxlcs.supabase.co/functions/v1/push_noti', 'public-anon-key')
-
-// serve(async (req) => {
-//   const { name } = await req.json()
-//   const data = {
-//     message: `Hello ${name}!`,
-//   }
-
-//   return new Response(
-//     JSON.stringify(data),
-//     { headers: { "Content-Type": "application/json" } },
-//   )
-// })
-
-// // To invoke:
-// curl -i --location --request POST 'https://rnkpkbfkrscmxfhjxlcs.supabase.co/functions/v1/push_noti'
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
-//   --header 'Content-Type: application/json'
-//   --data '{"name":"Functions"}'
-//   --body '{"app_id": "0566bb92-013b-4e29-aea9-af136bc546fd","included_segments": ["Subscribed Users"],"data": {"foo": "bar"},"contents": {"en": "Sample push Message"}}'
-
-// // To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
